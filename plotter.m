62e92b06f72d9cc00f5dff8befd55db870548626
Nicola Migone <nicolamigone179@gmail.com>
2025-12-13 12:31:02 +0100

    One graph per regulator

--- plotter.m
+++ plotter.m
@@ -43,39 +43,77 @@ lvr5_load_characteristics = table( ...
     [0; 38; 160; 443; 340; 54.2; 79; 186] ...
     );
 
-% TRANSFER CHAR GRAPH
-figure;
-hold on;
-plot(lvr1_transfer_characteristics.Var1, lvr1_transfer_characteristics.Var2, '-o', 'LineWidth', 2);
-plot(lvr2_transfer_characteristics.Var1, lvr2_transfer_characteristics.Var2, '-o', 'LineWidth', 2);
-plot(lvr5_transfer_characteristics.Var1, lvr5_transfer_characteristics.Var2, '-o', 'LineWidth', 2);
-hold off;
+% -- Regulator 1 --
+dataList(1).name = "lvr1"; 
+dataList(1).transfer = lvr1_transfer_characteristics;
+dataList(1).load = lvr1_load_characteristics;
 
-grid on;
-title('Transfer Characteristics for LVR1, LVR2, and LVR5');
-xlabel('U_{in} (V)');
-ylabel('U_{out} (V)');
-legend show;
+% -- Regulator 2 --
+dataList(2).name = "lvr2"; 
+dataList(2).transfer = lvr2_transfer_characteristics;
+dataList(2).load = lvr2_load_characteristics;
 
-saveas(gcf, "figures/transfer.png");
+% -- Regulator 5 --
+dataList(3).name = "lvr5"; 
+dataList(3).transfer = lvr5_transfer_characteristics;
+dataList(3).load = lvr5_load_characteristics;
 
-close;
 
-% NOLOAD CHAR GRAPH
+maxTransX = 0; maxTransY = 0;
+maxLoadX = 0;  maxLoadY = 0;
 
-figure;
-hold on;
-plot(lvr1_load_characteristics.Var2, lvr1_load_characteristics.Var1, '-o', 'LineWidth', 2);
-plot(lvr2_load_characteristics.Var2, lvr2_load_characteristics.Var1, '-o', 'LineWidth', 2);
-plot(lvr5_load_characteristics.Var2, lvr5_load_characteristics.Var1, '-o', 'LineWidth', 2);
-hold off;
+for i = 1:length(dataList)
+    % Find max for Transfer (Var1=X, Var2=Y)
+    maxTransX = max(maxTransX, max(dataList(i).transfer.Var1));
+    maxTransY = max(maxTransY, max(dataList(i).transfer.Var2));
+    
+    % Find max for Load/Output (Var2=X, Var1=Y)
+    maxLoadX = max(maxLoadX, max(dataList(i).load.Var2));
+    maxLoadY = max(maxLoadY, max(dataList(i).load.Var1));
+end
 
-grid on;
-title('Output Characteristics for LVR1, LVR2, and LVR5');
-xlabel('U_{in} (V)');
-ylabel('U_{out} (V)');
-legend show;
+paddingMultiplier = 1.10;
 
-saveas(gcf, "figures/output.png");
+globalTransLimsX = [0, maxTransX * paddingMultiplier];
+globalTransLimsY = [0, maxTransY * paddingMultiplier];
 
-close;
+globalLoadLimsX  = [0, maxLoadX * paddingMultiplier];
+globalLoadLimsY  = [0, maxLoadY * paddingMultiplier];
+
+for i = 1:length(dataList)
+    currentReg = dataList(i);
+    
+    % TRANSFER CHAR GRAPH
+    f = figure('Visible', 'off');
+    hold on;
+    plot(currentReg.transfer.Var1, currentReg.transfer.Var2, '-o', 'LineWidth', 2);
+    hold off;
+    grid on
+    xlim(globalTransLimsX);
+    ylim(globalTransLimsY);
+    title('Transfer Characteristics for regulator ' + upper(currentReg.name));
+    xlabel('U_{in} (V)');
+    ylabel('U_{out} (V)');
+    legend show;
+
+    saveas(f, "figures/transfer_" + currentReg.name + ".png");
+    close(f);
+
+    % NOLOAD CHAR GRAPH
+    f = figure('Visible', 'off');
+    hold on;
+    plot(currentReg.load.Var2, currentReg.load.Var1, '-o', 'LineWidth', 2);
+    hold off;
+    grid on;
+    xlim(globalLoadLimsX);
+    ylim(globalLoadLimsY);
+    title('Output Characteristics for regulator ' + upper(currentReg.name));
+    xlabel('U_{in} (V)');
+    ylabel('U_{out} (V)');
+    legend show;
+
+    saveas(f, "figures/load_" + currentReg.name + ".png");
+    close(f);
+
+    fprintf("Saved images for regulator %s\n", currentReg.name);
+end
